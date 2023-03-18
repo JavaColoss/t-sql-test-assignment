25e3c7a91a6c24ed6105ffd2f61ec783e0d07811
Islam Malikov <malikov56m@gmail.com>
2023-03-18 19:34:32 +0500

    Add files via upload

---
+++ CreateStructure.sql
@@ -0,0 +1,20 @@
+CREATE TABLE dbo.SKU (
+  ID INT IDENTITY(1,1) PRIMARY KEY,
+  Code AS 's' + CAST(ID AS VARCHAR(10)) UNIQUE,
+  Name VARCHAR(255)
+);
+
+CREATE TABLE dbo.Basket (
+  ID INT IDENTITY(1,1) PRIMARY KEY,
+  ID_SKU INT FOREIGN KEY REFERENCES dbo.SKU(ID),
+  Quantity INT CHECK (Quantity >= 0),
+  Value DECIMAL(10, 2) CHECK (Value >= 0),
+  PurchaseDate DATETIME DEFAULT(GETDATE()),
+  DiscountValue DECIMAL(10, 2)
+);
+
+CREATE TABLE dbo.Family (
+  ID INT IDENTITY(1,1) PRIMARY KEY,
+  SurName VARCHAR(255),
+  BudgetValue DECIMAL(10,2)
+);
